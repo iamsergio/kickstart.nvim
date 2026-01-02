ca7cc14b03174e8750193b61de68a592074dd6d0
Sergio Martins <@>
2026-01-02 10:53:22 +0000

    make tab use 4 spaces

--- init.lua
+++ init.lua
@@ -104,6 +104,12 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+-- Set tab width to 4 spaces
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
